af48b909e59b419c00d88c76bd230ec5ba42aa38
serwes <serwesbhatta@gmail.com>
2024-12-10 11:22:15 -0600

    fix: handle path for mkdir

--- Assignments/P01/cmd_pkg/mkdir.py
+++ Assignments/P01/cmd_pkg/mkdir.py
@@ -1,11 +1,16 @@
 from cmd_pkg.fs_state_manager import Fs_state_manager
+from .dir_path_helper import dir_path_helper
 from .call_api import call_api
 
 def mkdir(params = None):
     if params == None or len(params) == 0:
         return {"status": "fail", "message": "\nError: No directory name specified."}
 
-    directory_name = params[0]
+    directory_path = params[0]
+
+    directory_path_arr = directory_path.split("/")
+    directory_name = directory_path_arr.pop()
+    directory_path = "/".join(directory_path_arr)
 
     if "." in directory_name:
         return {
@@ -13,10 +18,24 @@ def mkdir(params = None):
             "message": "\nDirectory name cannot have extensions."
         }
 
-    current_pid = Fs_state_manager.get_pid()
-    oid = Fs_state_manager.get_oid()
+    try:
+        directory_path_response = dir_path_helper(directory_path)
+    except:
+        return {
+            "status": "fail",
+            "message": "\nCannot look for the specified directory."
+        }
+    
+    if directory_path_response["status"] == "fail":
+        return {
+            "status": "fail",
+            "message": f"\nError: {directory_path_response['message']}"
+        }
 
-    filters = {"oid": oid, "pid": current_pid, "name": directory_name}
+    pid = directory_path_response["pid"]
+    oid = directory_path_response["oid"]
+
+    filters = {"oid": oid, "pid": pid, "name": directory_name}
     existing_dir = call_api("dirs", params=filters)
 
     if existing_dir["status"] == "success":
