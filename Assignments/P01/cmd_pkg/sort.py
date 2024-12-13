0fe5f412626b08914986a5a8ab5628b111734a71
serwes <serwesbhatta@gmail.com>
2024-12-13 17:12:57 -0600

    fix: pipeline sort

--- Assignments/P01/cmd_pkg/sort.py
+++ Assignments/P01/cmd_pkg/sort.py
@@ -2,65 +2,76 @@ from .fs_state_manager import Fs_state_manager
 from .call_api import call_api
 from .file_path_helper import file_path_helper
 
-def sort(params=None):
-    if params == None:
+def sort(params=None, input = None):
+    if params == None and input == None:
         return {
             "status": "fail",
             "message": "\nPlease specify the filename or the filepath",
         }
     
-    file_path = params[0]
+    if input == None:
+        file_path = params[0]
     
-    file_path_response = file_path_helper(file_path)
+        file_path_response = file_path_helper(file_path)
 
-    if file_path_response["status"] == "fail":
-        return {
-            "status": "fail",
-            "message": "\nFile not found",
-        }
-    
-    oid = Fs_state_manager.get_oid()
-    pid = Fs_state_manager.get_pid()
-    file_name = file_path_response["file_name"]
-    
-    filters = {"oid": oid, "pid": pid, "name": file_name}
-
-    try:
-        get_response = call_api("files", params=filters)
-    except:
-        return {
-            "status": "fail",
-            "message": "\nCould not read file."
-        }
+        if file_path_response["status"] == "fail":
+            return {
+                "status": "fail",
+                "message": "\nFile not found",
+            }
         
-    if get_response["status"] == "success":
-        contents = get_response["message"][0]["contents"]
-        split_contents = contents.split("\n")
-        sorted_contents = sorted(split_contents)
-        sorted_output = "\n".join(sorted_contents)
-
-        filters = {"oid": oid, "pid": pid, "filepath": file_name, "content": sorted_output}
+        oid = Fs_state_manager.get_oid()
+        pid = Fs_state_manager.get_pid()
+        file_name = file_path_response["file_name"]
         
+        filters = {"oid": oid, "pid": pid, "name": file_name}
+
         try:
-            put_response = call_api("write", "put", data=filters)
+            get_response = call_api("files", params=filters)
         except:
             return {
                 "status": "fail",
-                "message": "\nCould not write in file."
+                "message": "\nCould not read file."
             }
+            
+        if get_response["status"] == "success":
+            contents = get_response["message"][0]["contents"]
+            split_contents = contents.split("\n")
+            sorted_contents = sorted(split_contents)
+            sorted_output = "\n".join(sorted_contents)
 
-        if put_response["status"] == "success":
-            return {
-                "status": "success",
-                "message": ""
-            }
+            filters = {"oid": oid, "pid": pid, "filepath": file_name, "content": sorted_output}
+            
+            try:
+                put_response = call_api("write", "put", data=filters)
+            except:
+                return {
+                    "status": "fail",
+                    "message": "\nCould not write in file."
+                }
+
+            if put_response["status"] == "success":
+                return {
+                    "status": "success",
+                    "message": ""
+                }
+            else:
+                return {
+                    "status": "fail",
+                    "message": "\nCould not write in file."
+                }
         else:
             return {
                 "status": "fail",
-                "message": "\nCould not write in file."
+                "message": "\nCould not read file."
             }
     else:
+        input_content = input["message"]
+        split_contents = input_content.split("\n")
+        sorted_contents = sorted(split_contents)
+        sorted_output = "\n".join(sorted_contents)
+
         return {
-            "status": "fail",
-            "message": "\nCould not read file."
+            "status": "success",
+            "message": sorted_output
         }
--- Assignments/P01/shell.py
+++ Assignments/P01/shell.py
@@ -55,7 +55,7 @@ def load_commands():
 def execute_command(main_cmd, args, input_data=None):
     if main_cmd in cmds:
         # Check if the command can accept input data and provide it if available
-        if input_data is not None and main_cmd in ["grep", "head", "tail", "wc"]:
+        if input_data is not None and main_cmd in ["grep", "head", "tail", "wc", "sort"]:
             result = cmds[main_cmd](params=args, input=input_data)
         else:
             result = cmds[main_cmd](params=args) if args else cmds[main_cmd]()
